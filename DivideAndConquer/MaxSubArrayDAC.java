f677261248f3c2d61ca213c384ac7b84e0e2ab45
Yogesh Darji <yogesh_darji99@yahoo.com>
2018-12-15 08:53:18 -0500

    commited Divide and conquer

---
+++ DivideAndConquer/MaxSubArrayDAC.java
@@ -0,0 +1,68 @@
+package DivideAndConquer;
+
+public class MaxSubArrayDAC {
+
+    public static int maxSubArray(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        return maxSubArray(nums, low, high);
+    }
+
+    public static int findMaximumCrossingSubArray(int nums[], int low, int mid, int high){
+        int leftSum = Integer.MIN_VALUE;
+        int sum = 0;
+        int maxLeft = 0;
+
+        for(int i=mid; i>=low; i--){
+            sum = sum + nums[i];
+            if(sum>leftSum){
+                leftSum = sum;
+                maxLeft = i;
+            }
+        }
+
+        int rightSum = Integer.MIN_VALUE;
+        sum = 0;
+        int maxRight = 0;
+
+        for(int i=mid+1; i<=high; i++){
+            sum = sum + nums[i];
+            if(sum>rightSum){
+                rightSum = sum;
+                maxRight = i;
+            }
+        }
+
+        return leftSum+rightSum;
+    }
+
+    private static int maxSubArray(int nums[], int low, int high){
+
+        int mid  = (low+high)/2;
+
+        if(low == high)
+            return nums[low];
+
+        else{
+            int leftSum = maxSubArray(nums, low, mid);
+            int rightSum = maxSubArray(nums, mid+1, high);
+            int crossSum = findMaximumCrossingSubArray(nums, low, mid, high);
+
+            if(leftSum >= rightSum && leftSum>=crossSum)
+                return leftSum;
+
+            else if(rightSum >= leftSum && rightSum >= crossSum)
+                return rightSum;
+
+            else{
+                return crossSum;
+            }
+        }
+
+    }
+
+    public static void main(String args[]){
+        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(arr));
+    }
+}
